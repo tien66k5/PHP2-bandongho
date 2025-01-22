db7352be59698f02d2661c4c494b88e1b85123f0
NhutTien <dangnhuttien1004@gmail.com>
2025-01-22 18:06:32 +0700

    assets template

--- Src/Views/Client/Home.php
+++ Src/Views/Client/Home.php
@@ -1,10 +1,11 @@
-
-
 <?php $this->layout('Client/Components/Layout');?>
 
 
 <?php $this->start('main_content') ?>
 <!-- Insert nội dung vào đây -->
+<?php
+$this->insert('/Client/Home/Home');
+?>
 
 
 <?php $this->stop() ?>
